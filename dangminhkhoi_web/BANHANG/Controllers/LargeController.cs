e07211be2f6319cb54aebf0114807b1addd739ef
minhkhoi2000 <khoi9362@gmail.com>
2023-03-09 11:19:49 +0700

    first commit

---
+++ dangminhkhoi_web/BANHANG/Controllers/LargeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BANHANG.Controllers
+{
+    public class LargeController : Controller
+    {
+        // GET: Large
+        public ActionResult Listing
+            ()
+        {
+            return View();
+        }
+    }
+}
