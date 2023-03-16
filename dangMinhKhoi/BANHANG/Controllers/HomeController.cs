718df566f2dee3cb70dac36903bf583dc8ce0497
minhkhoi2000 <khoi9362@gmail.com>
2023-03-16 11:28:15 +0700

    first commit

---
+++ dangMinhKhoi/BANHANG/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BANHANG.Conttext;
+using BANHANG.Models;
+
+namespace BANHANG.Controllers
+{
+    public class HomeController : Controller
+    {
+        webbanhangEntities objwebbanhangEntities = new webbanhangEntities();
+        public ActionResult Index()
+        {
+            HomeModel objHomeModel = new HomeModel();
+
+            objHomeModel.ListCategorys = objwebbanhangEntities.catetories.ToList();
+            objHomeModel.Listproducts = objwebbanhangEntities.products.ToList();
+            return View(objHomeModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ dangMinhKhoi/BANHANG/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using BANHANG.Conttext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BANHANG.Models
+{
+    public class HomeModel
+    {
+        public List<product> Listproducts { get; set; }
+        public List<catetory> ListCategorys { get; set; }
+    }
+}
